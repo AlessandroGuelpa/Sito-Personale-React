7d6595a5a651689c182799ef330b4bdb6fc8ee62
Alessandro Guelpa <alexander.guelpa@gmail.com>
2025-11-26 17:16:41 +0100

    modifica active

--- src/components/customlink.tsx
+++ src/components/customlink.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 // Definiamo i "tipi" che il componente accetta
 interface CustomLinkProps {
@@ -6,17 +7,22 @@ interface CustomLinkProps {
   className?: string;
   children: React.ReactNode; // Accetta testo, icone o altri elementi React
 }
-const baseClasses = "group relative inline-block font-medium cursor-pointer";
+
 const CustomLink = ({ href, children, className}: CustomLinkProps) => {
+  const baseClasses = "group relative inline-block font-medium cursor-pointer";
+  const finalClass = `${baseClasses} ${className || 'text-violet-600 dark:text-violet-500'}`;
+  const location = useLocation();
+  const isActive = location.pathname === href;
+
   return (
-    <a 
-      href={href} 
-      className={`${baseClasses} ${className || 'text-violet-600 dark:text-violet-500'}`}
-    >
+    <Link to={href} className={finalClass}>
       {children}
-      <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-violet-600 transition-all duration-300 ease-in-out group-hover:w-full"></span>
-    </a>
-  );
+      <span 
+        className={`absolute bottom-0 left-0 h-[2px] bg-indigo-600 transition-all duration-300 ease-in-out 
+        ${isActive ? 'w-full' : 'w-0 group-hover:w-full'}`}
+      ></span>
+    </Link>
+  )
 };
 
 export default CustomLink;
